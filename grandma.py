bd3a522f09f2bcba533ce24158ff67c5cbe3eaed
Ubuntu <ubuntu@ip-172-31-61-46.ec2.internal>
2022-04-05 17:41:23 +0000

    added grandma

---
+++ grandma.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Created by: Andrew Ten-Den
+# Created on: April 2022
+# This program lets the user see if grandma approves of them
+
+
+def main():
+    # this function lets the user see if grandma approves of them
+
+    # input
+    visual_status = input("Are you attractive (yes/no): ")
+    financial_status = input("Are you rich (yes/no): ")
+
+    # process & output
+    if visual_status == "yes" or financial_status == "yes":
+        print("You can date!")
+
+    elif visual_status == "no" and financial_status == "no":
+        print("No, you smell!")
+    else:
+        print("Please enter yes or no")
+
+    print("")
+    print("Done")
+
+
+if __name__ == "__main__":
+    main()
